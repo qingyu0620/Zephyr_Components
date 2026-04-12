a14a5b7dbe2cabef96510fbabe0e7c956c4a05b9
qingyu0620 <187825990+qingyu0620@users.noreply.github.com>
2026-04-13 00:33:44 +0800

    fix: System_startup.cpp add LED init and thread start

--- apps/System_startup.cpp
+++ apps/System_startup.cpp
@@ -10,6 +10,8 @@
  */
 
 #include "System_startup.h"
+#include "trd_led.h"
+#include <zephyr/drivers/gpio.h>
 
 void System_Bsp_Init()
 {
@@ -18,10 +20,22 @@ void System_Bsp_Init()
 
 void System_Modules_Init()
 {
-    
+    {
+        static const struct gpio_dt_spec spec[] {
+            GPIO_DT_SPEC_GET(DT_NODELABEL(led0), gpios),
+            GPIO_DT_SPEC_GET(DT_NODELABEL(led1), gpios),
+            GPIO_DT_SPEC_GET(DT_NODELABEL(led2), gpios),
+        };
+
+        for (uint8_t i = 0; i < thread::led::N; i++) {
+            Led ins{};
+            ins.Init(&spec[i]);
+            thread::led::thread_.Join(ins);
+        }
+    }
 }
 
 void System_Thread_Start()
 {
-    
+    thread::led::thread_start(5);
 }
