bb2979cfad39444af018853937e4010962c9b7f6
qingyu0620 <187825990+qingyu0620@users.noreply.github.com>
2026-04-13 16:45:05 +0800

    fix: empty System_startup.cpp for skeleton template

--- apps/System_startup.cpp
+++ apps/System_startup.cpp
@@ -1,17 +1,15 @@
 /**
  * @file System_startup.cpp
  * @author qingyu
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2026-04-07
- * 
+ *
  * @copyright Copyright (c) 2026
- * 
+ *
  */
 
 #include "System_startup.h"
-#include "trd_led.h"
-#include <zephyr/drivers/gpio.h>
 
 void System_Bsp_Init()
 {
@@ -20,22 +18,10 @@ void System_Bsp_Init()
 
 void System_Modules_Init()
 {
-    {
-        static const struct gpio_dt_spec spec[] {
-            GPIO_DT_SPEC_GET(DT_NODELABEL(led0), gpios),
-            GPIO_DT_SPEC_GET(DT_NODELABEL(led1), gpios),
-            GPIO_DT_SPEC_GET(DT_NODELABEL(led2), gpios),
-        };
 
-        for (uint8_t i = 0; i < thread::led::N; i++) {
-            Led ins{};
-            ins.Init(&spec[i]);
-            thread::led::thread_.Join(ins);
-        }
-    }
 }
 
 void System_Thread_Start()
 {
-    thread::led::thread_start(5);
+
 }
